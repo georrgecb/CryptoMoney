ea42447fdbfb48f43a889814cbbcda727861a979
georrgecb <bucurescu.george@gmail.com>
2022-01-18 22:16:35 +0200

    New API keys

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -48,8 +48,8 @@ const Cryptocurrencies = ({ simplified }) => {
         </>
       )}
       <Row gutter={[12, 12]} className="crypto-card-container">
-        {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={8} xl={6} key={currency.id}>
+        {cryptos?.map((currency, index) => (
+          <Col xs={24} sm={12} lg={8} xl={6} key={index}>
             <Link to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
